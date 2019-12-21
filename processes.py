69fd3c510a1c79e7d711cc2f5c111d15e1e7b782
Konrad Pazgan <249373@student.pwr.edu.pl>
2019-12-21 02:43:34 +0100

    load data finished check data in progress

--- processes.py
+++ processes.py
@@ -1,6 +1,22 @@
-class processes:
-    def __init__(self, napis):
-        tab = (napis.split())
-        self.duration = tab[0]
-        self.arrival = tab[1]
-        self.priority = tab[2]
+class Processes:
+    def __init__(self, tab):
+        self.duration = int(tab[0])  # czas trwania
+        self.arrival = int(tab[1])  # moment przybycia
+        self.priority = int(tab[2])  # priorytet
+
+    def display(self):
+        print(self.duration)
+        print(self.arrival)
+        print(self.priority)
+
+    def is_not_int(self):
+        if isinstance(self.duration, int) and isinstance(self.arrival, int) and isinstance(self.priority, int):
+            return False
+        else:
+            return True
+
+    def is_below_0(self):
+        if self.duration < 0 or self.arrival < 0 or self.priority < 0:
+            return True
+        else:
+            return False
--- untiled.py
+++ untiled.py
@@ -1,11 +1,53 @@
 import time
 import struct
-from processes import processes
+from typing import List, Any
 
-# czas trwania,moment przybycia,priorytet,
-dane_txt = open("dane.txt", mode="r+")
+from processes import Processes
 
-F = []
-for line in dane_txt:
-    F.append(processes(line))
-print(F[3].priority)
+
+def load_data_from_file(tab):
+    dane_txt = open("dane.txt", mode="r+")
+
+    for line in dane_txt:
+        try:
+            tab.append(Processes(line.split()))
+        except:
+            print('Dane w pliku są niepoprawne')
+    return
+
+
+def load_data_from_consol(tab):
+    print("Proszę wpisać informację o procesach")
+    next_process = bool(1)
+    while next_process:
+        process_parameters = []
+        string_parameters = ["Czas trwania: ","Czas przybycia: ", "Priorytet: "]
+        for step in range(0, 3):
+            process_parameters.append(input(string_parameters[step]))
+        while True:
+            print("Następny proces?(Wpisz T lub N)")
+            tmp = input()
+            if tmp == "T":
+                next_process = bool(1)
+                break
+            if tmp == "N":
+                next_process = bool(0)
+                break
+        tab.append(Processes(process_parameters))
+    return
+
+
+def check_data(tab):
+    for i in tab:
+        if i.is_not_int():
+            print("Dane powinny być liczbami całkowytymi")
+            return 1
+        if i.is_below_0():
+            print("Liczby powinny być nieujemne")
+            return 1
+    return
+
+
+tab_processes = []
+load_data_from_file(tab_processes)
+check_data(tab_processes)
